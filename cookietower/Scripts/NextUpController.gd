b6a3e8d69c2730e0f8ea97021f755ef8ebc39cf6
Ryan Merrick <ryan.merrick@outlook.com>
2025-08-25 15:34:33 +0100

    Adds `Next Up` functionality

--- cookietower/Scripts/NextUpController.gd
+++ cookietower/Scripts/NextUpController.gd
@@ -5,15 +5,34 @@ extends Node
 @export var nextUp02 : Sprite2D
 @export var nextUp03 : Sprite2D
 
+var nextUp : Array[PackedScene]
+
 func _ready() -> void:
-	var sce = targets.pick_random() as PackedScene
-	var inst = sce.instantiate() as Sprite2D
-	nextUp01.texture = inst.texture
+	nextUp.push_back(targets.pick_random() as PackedScene)
+	nextUp.push_back(targets.pick_random() as PackedScene)
+	nextUp.push_back(targets.pick_random() as PackedScene)
 	
-	sce = targets.pick_random() as PackedScene
-	inst = sce.instantiate() as Sprite2D
-	nextUp02.texture = inst.texture
+	updatePreview()
+
+func takeNextShape() -> PackedScene:
+	var next = nextUp.pop_front() as PackedScene
 	
-	sce = targets.pick_random() as PackedScene
-	inst = sce.instantiate() as Sprite2D
-	nextUp03.texture = inst.texture
+	nextUp.push_back(targets.pick_random() as PackedScene)
+	
+	updatePreview()
+	
+	return next
+	
+func updatePreview() -> void:
+	nextUp01.texture = (nextUp[0].instantiate() as Sprite2D).texture
+	nextUp01.cancel_free()
+	
+	nextUp02.texture = (nextUp[1].instantiate() as Sprite2D).texture
+	nextUp02.cancel_free()
+	
+	nextUp03.texture = (nextUp[2].instantiate() as Sprite2D).texture
+	nextUp03.cancel_free()
+
+func _input(event):
+	if Input.is_action_pressed("leftclick"):
+		takeNextShape()
